494238c90f5a72ea7ac84e21d00d8a831a2349b3
dansmog <juwonanthony@gmail.com>
2017-07-02 23:01:35 +0100

    add styling to our notepad app

--- src/App.js
+++ src/App.js
@@ -4,31 +4,87 @@ import './App.css';
 class App extends Component {
   render(){
     return(
-      <section>
+      <section className="wrapper">
           <header className="main-header">
-              <h1>Noted</h1>
-              <nav>
-                <a href="">Add New Note</a>
-                <a href="">Login</a>
-              </nav>
+             <div className="main-header__block">
+                <h1>Notepad</h1>
+                <nav>
+                  <a href=""><i className="icons icon-plus"></i>New note</a>
+                  <a href=""><i className="icons icon-login"></i>Login</a>
+                </nav>
+             </div>
+              
           </header> 
           <section className="notes">
 
+            <div className="notes__block">
+
+              <div className="noteCard">
+                <div className="noteCard--head">
+                  <h4>Test Note!</h4>
+
+                  <div className="noteCard--buttons">
+                      <i className="icons icon-pencil"></i>
+                      <i className="icons icon-close"></i>
+                  </div>
+                  
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio labouriosam disnissimos 
+                  voluptable nam quia tempore, delectus, consectetur adipiscing elit. Odio l. 
+                  Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+              </div>
+
+              <div className="noteCard">
+                <div className="noteCard--head">
+                  <h4>Test Note!</h4>
+
+                  <div className="noteCard--buttons">
+                      <i className="icons icon-pencil"></i>
+                      <i className="icons icon-close"></i>
+                  </div>
+                  
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio labouriosam disnissimos 
+                  voluptable nam quia tempore, delectus, consectetur adipiscing elit. Odio l. 
+                  Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+              </div>
+
+              <div className="noteCard">
+                <div className="noteCard--head">
+                  <h4>Test Note!</h4>
+
+                  <div className="noteCard--buttons">
+                      <i className="icons icon-pencil"></i>
+                      <i className="icons icon-close"></i>
+                  </div>
+                  
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio labouriosam disnissimos 
+                  voluptable nam quia tempore, delectus, consectetur adipiscing elit. Odio l. 
+                  Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+              </div>
+
+            </div>
+            
           </section>
+
           <aside className="sidebar">
               <form>
+                <h3>Add New Note</h3>
+                <i className="close">close</i>
                 <div className="form-group">
                   <label htmlFor="note-title">Title:</label>
                   <input type="text" name="note-title" />
                 </div>
                 <div className="form-group">
                   <label htmlFor="note-text">Text:</label>
-                  <textarea name="note" id="" cols="30" row="10"></textarea>
+                  <textarea name="note-text"></textarea>
                 </div>
 
-                
+                <button>Add New Note</button>
               </form>
           </aside>
+
       </section>
     )
   }
